7aeaf4cf6072cc7daa3552ab2ea33140d9548e0b
John Martin <jlemartin7@gmail.com>
2020-10-23 11:35:26 -0500

    did second part of exercise

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -21,7 +21,28 @@ namespace MethodExercise
             Console.WriteLine($"OK, {firstName}, your favorite color is {favColor}.");
             Console.WriteLine($"And your quest: {theQuest}.");
             Console.WriteLine($"And on Sunday, you will be attending {yourParish} for Mass.");
+            Console.WriteLine();
 
+            Console.WriteLine("Now, let's do some math.");
+            int firstNum = 3;
+            int secondNum = 4;
+
+            int newSum = Add(firstNum, secondNum);
+            Console.WriteLine($"The sum of the numbers is {newSum}.");
+
+            int newProduct = Multiply(firstNum, secondNum);
+            Console.WriteLine($"The product of the two numbers is {newProduct}.");
+
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
         }
     }
 }
