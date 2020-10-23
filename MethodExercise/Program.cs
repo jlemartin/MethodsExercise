eacb2c28ac697069522763b266eb5f4ab7d4aac8
John Martin <jlemartin7@gmail.com>
2020-10-23 11:17:04 -0500

    Finished first part of excercise

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine($"Hi {firstName}! What is your favorite color?");
+            string favColor = Console.ReadLine();
+
+            Console.WriteLine("What is your quest?");
+            string theQuest = Console.ReadLine();
+
+            Console.WriteLine("What is your parish?");
+            string yourParish = Console.ReadLine();
+
+            Console.WriteLine($"OK, {firstName}, your favorite color is {favColor}.");
+            Console.WriteLine($"And your quest: {theQuest}.");
+            Console.WriteLine($"And on Sunday, you will be attending {yourParish} for Mass.");
+
+        }
+    }
+}
